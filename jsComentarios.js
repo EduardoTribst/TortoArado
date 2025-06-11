86d4f5ad722074fa45b9a6a01d6f58a0d050d4b8
JulioPStein <juliopstein@gmail.com>
2025-06-10 22:10:10 -0300

    criar e mostrar comentarios

--- jsComentarios.js
+++ jsComentarios.js
@@ -1,7 +1,12 @@
-window.onload(carregarComentarios())
+const urlBase = "https://tortoarado.onrender.com/"
 
-function criarComentario() {
+window.onload = function () {
+    carregarComentarios();
+}
+
+async function criarComentario() {
     var usuario = document.getElementById("user").value;
+    var titulo = document.getElementById("titulo").value;
     var comentario = document.getElementById("textAreaComentario").value;
 
     if (usuario == "Rodrigo" || usuario == "rodrigo" || usuario == "Suess" || usuario == "Rodrigo Suess") {
@@ -9,8 +14,20 @@ function criarComentario() {
 
     }
 
-    if (usuario != "" && comentario != "") {
-        // post da imagem
+    if (usuario != "" && comentario != "" && titulo != "") {
+        var comentarioObj = {
+            autor: usuario,
+            titulo: titulo,
+            conteudo: comentario
+        };  
+
+        await fetch(urlBase + "Post/Criar", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(comentarioObj)
+        })
 
         carregarComentarios();
     }
@@ -19,6 +36,40 @@ function criarComentario() {
     }
 }
 
-function carregarComentarios() {
+async function carregarComentarios() {
+    const response = await fetch(urlBase + "Post/Selecionar");
+    const comentarios = await response.json();
 
+
+
+    var divComentarios = document.getElementById("divComentarios");
+    divComentarios.innerHTML = ""; // Limpa a lista antes de adicionar novos comentários
+
+    comentarios.forEach(comentario => {
+        divComentarios.innerHTML += `
+            <div class="comentario">
+                <h2>
+                    <span class="nomeUsuario">${comentario.autor}</span>
+                </h2>
+
+                <div class="corpoComentario">
+                    <h3>
+                        <span class="tituloComentario">${comentario.titulo}</span>
+                    </h3>
+
+                    <p class="textoComentario">
+                        ${comentario.conteudo}
+                    </p>
+                </div>
+                
+                <div class="likesPost">
+                    <button class="botaoVoto upvote" id="btnUpvotePost${comentario.id}" aria-label="Upvote">▲</button>
+
+                    <div class="contagemVoto" id="totalCountPost${comentario.id}">0</div>
+
+                    <button class="botaoVoto downvote" id="btnDownVotePost${comentario.id}" aria-label="Downvote">▼</button>
+                </div>
+            </div>
+            `
+    });
 }
